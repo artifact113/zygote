3e95c3fe347cfc0ef39000ba2bb66270ccc691d6
vijaymohanb4u <vijaymohanb4u@8a12a484-96f0-11de-9e26-af68eadc8346>
2009-09-03 13:09:59 +0000

    4

--- trunk/boot/boot.asm
+++ trunk/boot/boot.asm
@@ -14,7 +14,7 @@ org 0x7c00 			;Soon after the boot intel processors start
 
 start: jmp loader
 
-msg db "First Stage Boot Loader!!!", 0
+msg db "First Stage Boot Loader!!!",13,10,0
 
 Print:
 	lodsb
@@ -38,31 +38,30 @@ loader:
 
 	mov si, msg		;move the start address of string to si.
 
-	xor ax, ax
-	int 0x16		;get the amount of KB from the BIOS
+	call Print
 
-.reset:
-        mov ah, 0
-        mov dl, 0
-        int 13h
-        ;jc  .reset
+.Reset:
+	mov		ah, 0					; reset floppy disk function
+	mov		dl, 0					; drive 0 is floppy drive
+	int		0x13					; call BIOS
+	jc		.Reset					; If Carry Flag (CF) is set, there was an error. Try resetting again
 
-.read:
-        mov ax, 0x1000
-        mov es, ax
-        xor bx, bx
-        mov ah, 0x2
-        mov al, 1
-        mov ch, 1
-        mov cl, 1
-        mov dh, 0
-        mov dl, 0x0
-        int 13h
-        ;jc .read
+	mov		ax, 0x1000				; we are going to read sector to into address 0x1000:0
+	mov		es, ax
+	xor		bx, bx
 
- 	call Print
+.Read:
+	mov		ah, 0x02				; function 2
+	mov		al, 1					; read 1 sector
+	mov		ch, 1					; we are reading the second sector past us, so its still on track 1
+	mov		cl, 2					; sector to read (The second sector)
+	mov		dh, 0					; head number
+	mov		dl, 0					; drive number. Remember Drive 0 is floppy drive.
+	int		0x13					; call BIOS - Read the sector
+	jc		.Read					; Error, so try again
 
-	jmp 0x1000:0x00
+	jmp		0x1000:0x0				; jump to execute the sector!
+	
 
 times 510 - ($ - $$) db 0         ;Boot sector is of size 512 bytes. Fill the 
 				;space after the last instruction i.e.,hlt 
@@ -72,3 +71,6 @@ dw 0xAA55			;Boot signature (If the 511 byte is 0xAA
 				;and the 512 byte is 0x55, INT 0x19 will 
 				;load and execute the bootloader.).
 
+cli
+hlt
+times 1024 - ($ - $$) db 0
