fd08a7c522e16456870e2be17657013c9b05fdf3
vijaymohanb4u <vijaymohanb4u@8a12a484-96f0-11de-9e26-af68eadc8346>
2009-09-04 12:35:17 +0000

    5

--- trunk/boot/boot.asm
+++ trunk/boot/boot.asm
@@ -11,66 +11,86 @@ bits 16                         ;Intel processors starts in realmode
 org 0x7c00 			;Soon after the boot intel processors start 
 				;executing the Instructions from memory 
 				;location 0x7c00 
-
 start: jmp loader
 
-msg db "First Stage Boot Loader!!!",13,10,0
+msgLoading  db 0x0D, 0x0A, "Loading Boot Image ", 0x0D, 0x0A, 0x00
 
 Print:
-	lodsb
-	or al, al
-	jz PrintDone
-	mov ah, 0xe
-	int 10h
-	jmp Print
+			lodsb				; load next byte from string from SI to AL
+			or	al, al			; Does AL=0?
+			jz	PrintDone		; Yep, null terminator found-bail out
+			mov	ah, 0eh			; Nope-Print the character
+			int	10h
+			jmp	Print			; Repeat until null terminator found
+	PrintDone:
+			ret				; we are done, so return
 
-PrintDone:
-	ret
+Reset:
+        mov             ah, 0                                   ; reset floppy disk function
+        mov             dl, 0                                   ; drive 0 is floppy drive
+        int             0x13                                    ; call BIOS
+        jc              Reset                                   ; If Carry Flag (CF) is set, there was an error. Try rese
+        ret
+
+Read:
+        mov             ah, 0x02                                ; function 2
+        mov             al, 1                                   ; read 1 sector
+        mov             ch, 1                                   ; we are reading the second sector past us, so its still on track 1
+        mov             cl, 2                                   ; sector to read (The second sector)
+        mov             dh, 0                                   ; head number
+        mov             dl, 0                                   ; drive number. Remember Drive 0 is floppy drive.
+        int             0x13                                    ; call BIOS - Read the sector
+        jc              Read
+        ret
 
 ;-------------------------
 ;Boot Loader Entry Point -
 ;-------------------------
 loader:
-
+	;----------------------------------------------------
+     	; code located at 7C00:0000, adjust segment registers
+     	;----------------------------------------------------
+	cli  			;disable interrupts
 	xor ax, ax		;Make data segment & extended segments zero
 	mov ds, ax		;as the addresses are based on 0x7c00:0000
 	mov es, ax		;all the segments overlap with code segment.
+	mov fs, ax
+	mov gs, ax
 
-	mov si, msg		;move the start address of string to si.
+	;----------------------------------------------------
+     	; create stack
+     	;----------------------------------------------------
+     
+        mov     ax, 0x0000				; set the stack
+        mov     ss, ax
+        mov     sp, 0xFFFF
+        sti						; restore interrupts
 
-	call Print
 
-.Reset:
-	mov		ah, 0					; reset floppy disk function
-	mov		dl, 0					; drive 0 is floppy drive
-	int		0x13					; call BIOS
-	jc		.Reset					; If Carry Flag (CF) is set, there was an error. Try resetting again
+	;----------------------------------------------------
+	; Display loading message
+	;----------------------------------------------------
 
-	mov		ax, 0x1000				; we are going to read sector to into address 0x1000:0
+	mov si, msgLoading		;move the start address of string to si.
+	call Print     
+
+	call Reset
+
+	mov		ax, 0x0050				; we are going to read sector to into address 0x1000:0
 	mov		es, ax
 	xor		bx, bx
 
-.Read:
-	mov		ah, 0x02				; function 2
-	mov		al, 1					; read 1 sector
-	mov		ch, 1					; we are reading the second sector past us, so its still on track 1
-	mov		cl, 2					; sector to read (The second sector)
-	mov		dh, 0					; head number
-	mov		dl, 0					; drive number. Remember Drive 0 is floppy drive.
-	int		0x13					; call BIOS - Read the sector
-	jc		.Read					; Error, so try again
+	call Read
 
-	jmp		0x1000:0x0				; jump to execute the sector!
-	
+	jmp 0x0050:0x0000	
 
-times 510 - ($ - $$) db 0         ;Boot sector is of size 512 bytes. Fill the 
+times 510 - ($ - $$) db 0       ;Boot sector is of size 512 bytes. Fill the 
 				;space after the last instruction i.e.,hlt 
 				;until byte 510 with '0'.
 
 dw 0xAA55			;Boot signature (If the 511 byte is 0xAA 
 				;and the 512 byte is 0x55, INT 0x19 will 
 				;load and execute the bootloader.).
-
-cli
-hlt
+	cli 
+	hlt
 times 1024 - ($ - $$) db 0
